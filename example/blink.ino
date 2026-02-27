7e2780c55fb86fa4b7bac09a51576521e2c1a8f8
AxisFab <axisfab2026@gmail.com>
2026-02-27 18:09:37 +0900

    Create blink.ino

---
+++ example/blink.ino
@@ -0,0 +1,20 @@
+/*******************************************************
+ *  AXISFAB BRICKS-ESP Sensor Example Code
+ *  Sensor Module : Blink LED(Built-in Device)
+ *  Example: Blink LED Example
+ *  Version : Bricks_sensor v1.0
+ *******************************************************/
+
+#define LED_PIN 2  //Blink LED(D4)
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  digitalWrite(LED_PIN, HIGH);
+  delay(500);
+  digitalWrite(LED_PIN, LOW);
+  delay(500);
+}
